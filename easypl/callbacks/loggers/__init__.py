783e67268b8242deefc86ff7531dec0553500296
data_sanchez <tam2511@mail.ru>
2022-03-10 19:21:36 +0300

    add exist loggers

---
+++ easypl/callbacks/loggers/__init__.py
@@ -0,0 +1,4 @@
+from easypl.callbacks.loggers.classification import ClassificationImageLogger
+from easypl.callbacks.loggers.segmentation import SegmentationImageLogger
+from easypl.callbacks.loggers.base import BaseSampleLogger
+from easypl.callbacks.loggers.base_image import BaseImageLogger
