10e30330021d80885bed893f69319d78a0e7a588
data_sanchez <tam2511@mail.ru>
2022-03-13 14:04:34 +0300

    add mosaic

--- easypl/callbacks/mixers/__init__.py
+++ easypl/callbacks/mixers/__init__.py
@@ -1,3 +1,4 @@
 from easypl.callbacks.mixers.base import MixBaseCallback
 from easypl.callbacks.mixers.cutmix import Cutmix
 from easypl.callbacks.mixers.mixup import Mixup
+from easypl.callbacks.mixers.mosaic import Mosaic
